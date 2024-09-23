9f2234f4301c5b98245bebf21dc238b7792a31e9
Matthieu HONORÉ <31983929+unkatorz@users.noreply.github.com>
2024-09-23 09:03:21 +0000

    ex07vfinal

--- ex07/test.py
+++ ex07/test.py
@@ -7,11 +7,11 @@ from main import frontal_function, MySpecificException
 def testFrontalFunction(): # Rename accordingly
     
     ### Nominal Cases
-    assert frontal_function(...) == ...
+    assert frontal_function("Je vais sur un bateau qui est sur la mer") == "bateau:1,est:1,je:1,la:1,mer:1,qui:1,sur:2,un:1,vais:1"
     ###
     
     
     ### Edge cases and corner cases
-    with pytest.raises(...):
-        frontal_function(...)
+    with pytest.raises():
+        frontal_function("")
     ###
