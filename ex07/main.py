c1d84a89cd239d2cbbb7265b3255d40deb39ec84
Matthieu HONORÉ <31983929+unkatorz@users.noreply.github.com>
2024-09-23 09:03:04 +0000

    ex07vfinal

--- ex07/main.py
+++ ex07/main.py
@@ -9,32 +9,51 @@ class BadInputException(ValueError):
     pass
 
 
-def frontal_function() :
+def frontal_function(liste_mots) :
     """
     Rename and document this function accordingly.
     
         Parameters:
-            
+            liste_mots : array
         
         Raises:
             
         
         Returns:
+            result: str ; "mot1:frequence,mot2:frequence,mot_n:frequence"
             
     """
+
+    result = ""
+
+    frequence_mots = {}
+
+    for mot in liste_mots:
+        if mot in frequence_mots:
+            frequence_mots[mot] += 1
+        else:
+            frequence_mots[mot] = 1
     
-    return 
+    result = ','.join([f"{mot}:{frequence}" for mot, frequence in frequence_mots.items()])
+
+    return result
 
 def main(line: str):
     
     ### Line parsing and BAD INPUT checking
     
     ###
-    
+    new_line = line.lower()
+    phrase = new_line.split(" ")
+    for mots in phrase:
+        if not re.match(r"^[a-zA-Z]+$", mots): raise BadInputException()
+    phrase.sort()
     
     ### Frontal function call and exceptions management
     
     ###
+    result = frontal_function(phrase)
+    print(result)
 
 
 if __name__ == "__main__":
